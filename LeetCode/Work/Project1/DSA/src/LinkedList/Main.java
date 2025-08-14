a5e8900b0ff5511b092f8ca0d687324de97fa24e
ShailuGithub <shailu580@gmail.com>
2025-08-14 09:58:20 +0530

    Add files via upload

--- LeetCode/Work/Project1/DSA/src/LinkedList/Main.java
+++ LeetCode/Work/Project1/DSA/src/LinkedList/Main.java
@@ -12,17 +12,18 @@ package LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-//        LL list = new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(2);
-//        list.insertFirst(8);
-//        list.insertFirst(17);
-//        list.InsertLast(99);
-//        list.insert(100, 3); 
-//        //System.out.println(list.deleteFirst());
-//        //System.out.println(list.deleteLast()); 
-//        System.out.println(list.delete(2));
-//        list.display();
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.InsertLast(99);
+        list.insert(100, 3); 
+        System.out.println(list.deleteFirst());
+        System.out.println(list.deleteLast()); 
+        System.out.println(list.delete(2));
+        list.insertRec(88, 2);
+        list.display();
 
 //        DLL list = new DLL();
 //        list.insertFirst(3);
@@ -32,13 +33,13 @@ public class Main {
 //        list.insertLast(99);
 //        list.insert(8, 65);
 //        list.display();
-        CLL list = new CLL();
-        list.insert(23);
-        list.insert(3);
-        list.insert(19);
-        list.insert(75);
-        list.display();
-        list.delete(19);
-        list.display();
+//        CLL list = new CLL();
+//        list.insert(23);
+//        list.insert(3);
+//        list.insert(19);
+//        list.insert(75);
+//        list.display();
+//        list.delete(19);
+//        list.display();
     }
 }
